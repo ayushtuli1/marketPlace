1497b003875669e6034d1101ea2b0243a7e3c571
Ayush Tuli <ayushtuli1@gmail.com>
2021-11-20 22:43:31 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import "../App.css";
 
 export default function Login(props) {
   const { setLogin } = props;
+  const [userName ,setUserName]= useState('')
+  const [error ,setError]= useState('')
+
+  const [password ,setPassword]= useState('')
+
+  const isAuthenticated =()=>{
+    console.log("isAuthenticated")
+    if(userName ==='admin' && password ==='admin'){
+      setLogin(true)
+    }else{
+      setError('UserName or password incorrect')
+    }
+  }
  
   return (
         <div className="wrapper fadeInDown">
@@ -11,11 +25,19 @@ export default function Login(props) {
            </div> 
            <div id="formContent">
             <img className="company-logo" alt="notch" src="https://storage.googleapis.com/chefhero-storage/static/fe-buyer/images/notch-primary-logo.svg" />
-            <form>
-              <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
-              <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
-              <input type="submit" onClick={()=>{setLogin(true)}} className="fadeIn fourth" value="Log In" />
-            </form>
+            
+              <input type="text" id="login" autoComplete={false} onChange={(ev)=>{
+                setUserName(ev.target.value)
+              }} className="fadeIn second" name="login" placeholder="login" />
+              <input type="text" id="password"  onChange={(ev)=>{
+                setPassword(ev.target.value)  }} className="fadeIn third" name="login" placeholder="password" />
+              <input type="button" onClick={isAuthenticated} className="fadeIn fourth" value="Log In" />
+              {error !== '' &&
+                <div  id="formFooter" className="error">
+                  {error}
+                </div> 
+              }
+            
            
             <div id="formFooter">
               <a className="underlineHover" href="#">Forgot Password?</a>
